00da13927455f4b54fcc36b57f16ed140ad504cd
Ivan Bruel <ivan.bruel@gmail.com>
2016-03-18 16:52:03 +0000

    Initial Commit

---
+++ Build/Intermediates/KittenKit.build/Debug-iphonesimulator/KittenKit.build/DerivedSources/KittenKit_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char KittenKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:KittenKit  PROJECT:KittenKit-1" "\n"; const double KittenKitVersionNumber __attribute__ ((used)) = (double)1.;
